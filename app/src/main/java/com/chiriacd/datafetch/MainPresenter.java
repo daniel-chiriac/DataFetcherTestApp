e500680d49fad2461b8cd7505cc9a7028b7a9efc
Daniel Chiriac <d4ny90@yahoo.com>
2018-02-26 22:28:27 +0200

    Fix mistake of using double subscribe

--- app/src/main/java/com/chiriacd/datafetch/MainPresenter.java
+++ app/src/main/java/com/chiriacd/datafetch/MainPresenter.java
@@ -12,10 +12,8 @@ import java.net.SocketTimeoutException;
 
 import javax.inject.Inject;
 
-import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainPresenter {
@@ -38,21 +36,18 @@ public class MainPresenter {
         this.nextPageService = nextPageService;
         codesManager = new CodesManager(dataStore.getCodeMap());//todo perhaps should be moved from here
         compositeDisposable = new CompositeDisposable();
-
     }
 
     public void refreshData() {
         compositeDisposable.add(rootFetchService.getRoot()
                 .subscribeOn(Schedulers.io())
+                .flatMap(rootPage -> nextPageService.getNextPath(rootPage.getNextPath()))
+                .map(NextPath::getResponseCode)
+                .doOnNext(codesManager::add)
+                .map(codesManager.getData()::get)
+                .doOnNext(response -> lastResponse = response)
                 .observeOn(AndroidSchedulers.mainThread())
-                .map(rootPage -> nextPageService.getNextPath(rootPage.getNextPath()))
-                .subscribe(nextPathObservable -> nextPathObservable.subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .map(NextPath::getResponseCode)
-                        .doOnNext(codesManager::add)
-                        .map(codesManager.getData()::get)
-                        .doOnNext(response -> lastResponse = response)
-                        .subscribe(mainView::updateResponseView, this::handleRootFetchErrors), this::handleRootFetchErrors));
+                .subscribe(mainView::updateResponseView, this::handleRootFetchErrors));
     }
 
     public Response getLastResponse() {
